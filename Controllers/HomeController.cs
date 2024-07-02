fc1e94b90ca2d6c95f8d3e4a33916a5a45c74858
Kurban KOTAN <kurbankotan@gmail.com>
2024-07-02 09:43:04 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FormsApp.Models;
+
+namespace FormsApp.Controllers;
+
+public class HomeController : Controller
+{
+
+
+    public HomeController()
+    {
+
+    }
+
+    public IActionResult Index(string searchString)
+    {
+        var products = Repository.Products;
+        if(!String.IsNullOrEmpty(searchString))
+        {
+            products = products.Where(p=>p.Name.ToLower().Contains(searchString)).ToList();
+
+        }
+        return View(products);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,42 @@
+namespace FormsApp.Models
+{
+    public class Repository
+    {
+        private static readonly List<Product> _products = new();
+
+        public static List<Product> Products
+        {
+            get{
+                return _products;
+            }
+        }
+
+
+
+        private static readonly List<Category> _categories = new();
+        public static List<Category> Categories
+        {
+            get{
+                return _categories;
+            }
+        }
+
+
+        //Static Contructor
+        static Repository()
+        {
+            _categories.Add(new Category {CategoryId=1, Name="Telefon"});
+            _categories.Add(new Category {CategoryId=1, Name="bilgisayar"});
+
+            _products.Add(new Product{ProductId=1, Name = "Iphone 14", Price=4000, IsActive=true, Image="1.jpg", CategoryId=1 });
+            _products.Add(new Product{ProductId=2, Name = "Iphone 15", Price=5000, IsActive=true, Image="2.jpg", CategoryId=1 });
+            _products.Add(new Product{ProductId=3, Name = "Iphone 16", Price=6000, IsActive=true, Image="3.jpg", CategoryId=1 });
+            _products.Add(new Product{ProductId=4, Name = "Iphone 17", Price=7000, IsActive=true, Image="4.jpg", CategoryId=1 });
+
+
+            _products.Add(new Product{ProductId=5, Name = "Macbook Air", Price=8000, IsActive=true, Image="5.jpg", CategoryId=2 });
+            _products.Add(new Product{ProductId=6, Name = "Macbook Pro", Price=9000, IsActive=true, Image="6.jpg", CategoryId=2 });
+        }
+
+    }
+}
